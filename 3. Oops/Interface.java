6e8312df9027284b09e0220af296048dea84247d
Archana Kumari <archanakumari8n7@gmail.com>
2022-07-17 00:48:49 +0530

    Update Interface.java

--- 3. Oops/Interface.java
+++ 3. Oops/Interface.java
@@ -1,1 +1,18 @@
 
+package oops;
+interface Animals{
+	public void walk();
+}
+
+class Horses implements Animals{
+	public void walk() {
+		System.out.println("Walks on 4 legs");
+	}
+}
+
+public class Interface {
+    public static void main() {
+    	Horses horse = new Horses();
+    	horse.walk();
+    }
+}
