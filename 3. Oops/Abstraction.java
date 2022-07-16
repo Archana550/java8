1559fb1d5272647dd923a2425e62fb2df1fd5481
Archana Kumari <archanakumari8n7@gmail.com>
2022-07-17 00:34:40 +0530

    Update Abstraction.java

--- 3. Oops/Abstraction.java
+++ 3. Oops/Abstraction.java
@@ -1,12 +1,25 @@
 package oops;
 
 abstract class Animal{
+	//abstract methods
 	abstract void walk();
-		
 	
+	//non-abstract methods
+	public void eats() {
+		System.out.println("Eats");
+	}
+	
+	//constructor
+	Animal(){
+		System.out.println("You are creating an animal");
+	}
 }
 
 class Horse extends Animal{
+	
+	Horse(){
+		System.out.println("You are creating an Horse");
+	}
 	public void walk() {
 		System.out.println("Walks on 4 legs");
 	}
@@ -22,7 +35,8 @@ public class Abstraction {
    public static void main() {
 	   Horse horse = new Horse();
 	   horse.walk();
-	   Animal animal = new Animal();
-	   animal.walk();
+	   horse.eats();
+	  // Animal animal = new Animal();
+	  // animal.walk();
    }
 }
