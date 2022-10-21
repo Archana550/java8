3bf77f43f5d5ff332a4bd1dd8d8fa16e5c4ed859
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:57:09 +0530

    Update ApiException.java

--- BlogAppApi/src/main/java/com/rest/blog/exception/ApiException.java
+++ BlogAppApi/src/main/java/com/rest/blog/exception/ApiException.java
@@ -1,1 +1,16 @@
 
+package com.rest.blog.exception;
+
+public class ApiException extends RuntimeException {
+
+private static final long serialVersionUID = 1L;
+public ApiException(String message) {
+super(message);
+
+}
+public ApiException() {
+super();
+
+}
+
+}
