c242a7382806e66ff0a07e6d1f9cc7748ef5b956
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:57:34 +0530

    Update EmailAlreadyRegisteredExeception.java

--- BlogAppApi/src/main/java/com/rest/blog/exception/EmailAlreadyRegisteredExeception.java
+++ BlogAppApi/src/main/java/com/rest/blog/exception/EmailAlreadyRegisteredExeception.java
@@ -1,1 +1,17 @@
 
+package com.rest.blog.exception;
+
+public class EmailAlreadyRegisteredException extends RuntimeException {
+
+
+private static final long serialVersionUID = 1L;
+
+public EmailAlreadyRegisteredException(String message) {
+super(message);
+
+}
+
+
+
+
+}
