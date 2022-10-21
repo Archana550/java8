60d5c812913b2a4c1c55b50ad2e1cb6b425ad7e6
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:11:19 +0530

    Update PostService.java

--- BlogAppApi/src/main/java/com/rest/blog/services/PostService.java
+++ BlogAppApi/src/main/java/com/rest/blog/services/PostService.java
@@ -1,1 +1,33 @@
 
+package com.rest.blog.services;
+
+import java.util.List;
+
+import com.rest.blog.entities.Post;
+import com.rest.blog.payloads.PostDto;
+
+public interface PostService {
+
+/** Create post */
+PostDto createPost(PostDto postDto, Integer userId, Integer categoryId);
+
+
+/** Update post */
+PostDto updatePost(PostDto postDto, Integer postId);
+
+/** Delete post */
+Post deletePost(Integer postId);
+
+/** Get post */
+PostDto getPostById(Integer postId);
+
+/** Get post by category */
+List<PostDto> getPostsByCategory(Integer categoryId);
+
+/** Get post by user */
+List<PostDto> getPostsByUser(Integer userId);
+
+/**Get all post */
+List<PostDto> getAllPost(Integer pageNumber, Integer pageSize);
+
+}
