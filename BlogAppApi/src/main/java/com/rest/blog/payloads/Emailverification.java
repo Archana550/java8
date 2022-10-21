70e6a26c698e8f4c6112c914b22305d577dd1e4a
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:02:02 +0530

    Update Emailverification.java

--- BlogAppApi/src/main/java/com/rest/blog/payloads/Emailverification.java
+++ BlogAppApi/src/main/java/com/rest/blog/payloads/Emailverification.java
@@ -1,1 +1,20 @@
 
+package com.rest.blog.payloads;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Emailverification {
+@NotNull(message="Email can't be empty.")
+@Email
+private String email;
+@NotNull
+private int otp;
+}
