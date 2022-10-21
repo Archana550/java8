1082ecf3949415ddcee613a4a2553dd5a2866a17
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:03:01 +0530

    Update CategoryDto.java

--- BlogAppApi/src/main/java/com/rest/blog/payloads/CategoryDto.java
+++ BlogAppApi/src/main/java/com/rest/blog/payloads/CategoryDto.java
@@ -24,24 +24,3 @@ private String categoryTitle;
 private String categoryDescription;
 
 }
-package com.rest.blog.payloads;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.hateoas.RepresentationModel;
-
-public class CategoryList extends RepresentationModel<CategoryList>{
-
-private static final long serialVersionUID = 1L;
-
-   private List<CategoryDto> categories = new ArrayList<CategoryDto>();
-
-   public List<CategoryDto> getCategories() {
-       return categories;
-   }
-
-   public void setCategories(List<CategoryDto> categories) {
-       this.categories = categories;
-   }
-}
