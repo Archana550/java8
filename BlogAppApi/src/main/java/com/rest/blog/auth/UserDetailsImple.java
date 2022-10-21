cac13c1661a8884028c92138098b2b293faf8440
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:29:24 +0530

    Update and rename serDetailsImple.java to UserDetailsImple.java

---
+++ BlogAppApi/src/main/java/com/rest/blog/auth/UserDetailsImple.java
@@ -0,0 +1,76 @@
+
+package com.rest.blog.auth;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.rest.blog.entities.User;
+
+
+@Service
+public class UserDetailsImple implements UserDetails{
+
+
+private static final long serialVersionUID = 1L;
+
+private User user;
+
+public UserDetailsImple( ) {
+
+}
+public UserDetailsImple(User user) {
+super();
+this.user = user;
+}
+@Override
+public Collection<? extends GrantedAuthority> getAuthorities() {
+HashSet<SimpleGrantedAuthority> set = new HashSet<>();
+set.add(new SimpleGrantedAuthority(user.getRole().getName()));
+
+return set;
+
+}
+
+@Override
+public String getPassword() {
+
+return user.getPassword();
+}
+
+@Override
+public String getUsername() {
+ 
+return user.getEmail();
+}
+
+@Override
+public boolean isAccountNonExpired() {
+
+return true;
+}
+
+@Override
+public boolean isAccountNonLocked() {
+
+return true;
+}
+
+@Override
+public boolean isCredentialsNonExpired() {
+
+return true;
+}
+
+@Override
+public boolean isEnabled() {
+
+return user.isEnabled();
+}
+
+}
+
