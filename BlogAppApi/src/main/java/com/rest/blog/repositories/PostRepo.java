8b4efdcaf24725d7a7c845b9955eed6755617d5e
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:07:26 +0530

    Update PostRepo.java

--- BlogAppApi/src/main/java/com/rest/blog/repositories/PostRepo.java
+++ BlogAppApi/src/main/java/com/rest/blog/repositories/PostRepo.java
@@ -1,1 +1,23 @@
+package com.rest.blog.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rest.blog.entities.Category;
+import com.rest.blog.entities.Post;
+import com.rest.blog.entities.User;
+
+public interface PostRepo extends JpaRepository<Post, Integer> {
+
+List<Post> findByUser(User user);
+
+List<Post> findByCategory(Category category);
+
+}
+
+
+
+
+
 
