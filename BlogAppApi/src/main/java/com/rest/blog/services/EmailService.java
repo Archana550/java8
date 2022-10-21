5b4e92897307496b6096cd57d58c28ebd51c7cd6
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:10:48 +0530

    Update EmailService.java

--- BlogAppApi/src/main/java/com/rest/blog/services/EmailService.java
+++ BlogAppApi/src/main/java/com/rest/blog/services/EmailService.java
@@ -1,1 +1,15 @@
 
+package com.rest.blog.services;
+
+import java.util.ArrayList;
+
+import com.rest.blog.entities.User;
+
+public interface EmailService {
+
+boolean sendSimpleMail(ArrayList<String> newsletter, int postId);
+
+int sendOTPforEmailVerification(User user);
+}
+
+
