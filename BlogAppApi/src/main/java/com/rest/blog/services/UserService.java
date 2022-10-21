30dc175834cf7224f709bd5e65b66e300395dd97
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:11:40 +0530

    Update UserService.java

--- BlogAppApi/src/main/java/com/rest/blog/services/UserService.java
+++ BlogAppApi/src/main/java/com/rest/blog/services/UserService.java
@@ -1,1 +1,19 @@
 
+package com.rest.blog.services;
+
+
+
+import java.util.List;
+
+import com.rest.blog.payloads.UserDto;
+
+
+public interface UserService {
+
+
+UserDto updateUser(UserDto user, Integer userId);
+UserDto getUserById(Integer userId);
+List<UserDto> getAllUsers();
+void deleteUser(Integer userId);
+}
+
