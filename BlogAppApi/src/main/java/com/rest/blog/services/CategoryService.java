f5f6b923c875d46cf99b27fe5065dfcdacddb0fa
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:10:10 +0530

    Update CategoryService.java

--- BlogAppApi/src/main/java/com/rest/blog/services/CategoryService.java
+++ BlogAppApi/src/main/java/com/rest/blog/services/CategoryService.java
@@ -1,1 +1,29 @@
 
+package com.rest.blog.services;
+
+import java.util.List;
+
+import com.rest.blog.entities.Category;
+import com.rest.blog.payloads.CategoryDto;
+
+public interface CategoryService {
+
+
+//create
+CategoryDto createCategory(CategoryDto categoryDto);
+//update
+CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId);
+//delete
+ Category deleteCategory(Integer categoryId);
+
+//get
+ CategoryDto getCategory(Integer categoryId);
+//getAll
+ 
+ List<CategoryDto> getCategories();
+}
+
+
+
+
+
