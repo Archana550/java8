4f40771697c07559639671a1653f005d8bc2cc1c
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:07:51 +0530

    Update RoleRepo.java

--- BlogAppApi/src/main/java/com/rest/blog/repositories/RoleRepo.java
+++ BlogAppApi/src/main/java/com/rest/blog/repositories/RoleRepo.java
@@ -1,1 +1,11 @@
 
+package com.rest.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rest.blog.entities.Role;
+
+public interface RoleRepo extends JpaRepository<Role, Integer> {
+
+}
+
