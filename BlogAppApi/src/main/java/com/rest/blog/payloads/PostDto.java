355b3cb0d9ba8350752eb7c296e8488a6400bed2
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:04:06 +0530

    Update PostDto.java

--- BlogAppApi/src/main/java/com/rest/blog/payloads/PostDto.java
+++ BlogAppApi/src/main/java/com/rest/blog/payloads/PostDto.java
@@ -1,1 +1,36 @@
 
+package com.rest.blog.payloads;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDto extends RepresentationModel<PostDto>{
+
+private Integer postId;
+@Size(min=4, message="Title must be 4 characters long.")
+@NotBlank(message="Title is mandatory")
+private String title;
+@Size(min=4, message="Content must be 4 characters long.")
+@NotBlank(message="Content is mandatory")
+private String content;
+
+private Date createdAt;
+private Integer categoryId;
+private CategoryDto category;
+private UserDto user;
+
+}
