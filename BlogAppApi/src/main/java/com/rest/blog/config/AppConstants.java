41e5e234e8d3a85f11aa9ed1bf1f53d088675360
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:38:54 +0530

    Update AppConstants.java

--- BlogAppApi/src/main/java/com/rest/blog/config/AppConstants.java
+++ BlogAppApi/src/main/java/com/rest/blog/config/AppConstants.java
@@ -1,1 +1,13 @@
 
+package com.rest.blog.config;
+
+public class AppConstants {
+
+public static final String PAGE_NUMBER="0";
+public static final String PAGE_SIZE="2";
+public static final String SORT_BY="postId";
+
+public static final String SECRET_KEY="laksdjbyuregjbcngjpyewjg";
+public static final Integer NORMAL_USER=0;
+public static final Integer ADMIN=1;
+}
