3aaa97f474b34c9b44bd43f5d640cbbd45796db5
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:08:06 +0530

    Update UserRepo.java

--- BlogAppApi/src/main/java/com/rest/blog/repositories/UserRepo.java
+++ BlogAppApi/src/main/java/com/rest/blog/repositories/UserRepo.java
@@ -1,1 +1,13 @@
 
+package com.rest.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.rest.blog.entities.User;
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+User findByEmail(String email);
+}
