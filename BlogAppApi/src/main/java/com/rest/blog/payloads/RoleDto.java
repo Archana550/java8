1fe4f9d0107c567c77f9030c11a7f0d94d2e7ab5
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:06:08 +0530

    Update RoleDto.java

--- BlogAppApi/src/main/java/com/rest/blog/payloads/RoleDto.java
+++ BlogAppApi/src/main/java/com/rest/blog/payloads/RoleDto.java
@@ -1,1 +1,18 @@
 
+
+package com.rest.blog.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class RoleDto {
+private int id;
+private String name;
+}
