7be5a3d70c7c4c42d18b87da40f7bbe7f4471824
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:07:04 +0530

    Update CategoryRepo.java

--- BlogAppApi/src/main/java/com/rest/blog/repositories/CategoryRepo.java
+++ BlogAppApi/src/main/java/com/rest/blog/repositories/CategoryRepo.java
@@ -1,1 +1,12 @@
 
+package com.rest.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rest.blog.entities.Category;
+
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
+
+}
+
+
