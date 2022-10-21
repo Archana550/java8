0453d3a15a08d6bf15d5dd404b0cc062726ab5f5
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:56:39 +0530

    Update AccessNotPrmitedException.java

--- BlogAppApi/src/main/java/com/rest/blog/exception/AccessNotPrmitedException.java
+++ BlogAppApi/src/main/java/com/rest/blog/exception/AccessNotPrmitedException.java
@@ -1,1 +1,15 @@
 
+package com.rest.blog.exception;
+
+public class AccessNotPermitedException extends RuntimeException {
+
+
+private static final long serialVersionUID = 1L;
+
+public AccessNotPermitedException(String message) {
+super(message);
+
+}
+
+
+}
