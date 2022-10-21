02020040feff8ade9261b8b34eac26d33b45b8fe
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 13:04:45 +0530

    Update PostList.java

--- BlogAppApi/src/main/java/com/rest/blog/payloads/PostList.java
+++ BlogAppApi/src/main/java/com/rest/blog/payloads/PostList.java
@@ -1,1 +1,21 @@
 
+package com.rest.blog.payloads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class PostList extends RepresentationModel<PostList> {
+private static final long serialVersionUID = 1L;
+
+   private List<PostDto> posts = new ArrayList<>();
+
+   public List<PostDto> getPosts() {
+       return posts;
+   }
+
+   public void setPosts(List<PostDto> posts) {
+       this.posts = posts;
+   }
+}
