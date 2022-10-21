76a7ffd1d7f14564882cd27848765aaa0b8c85cf
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:59:23 +0530

    Update UserNotFoundException.java

--- BlogAppApi/src/main/java/com/rest/blog/exception/UserNotFoundException.java
+++ BlogAppApi/src/main/java/com/rest/blog/exception/UserNotFoundException.java
@@ -1,1 +1,13 @@
 
+package com.rest.blog.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+private static final long serialVersionUID = 1L;
+
+public UserNotFoundException(String message) {
+super(message);
+
+}
+
+}
