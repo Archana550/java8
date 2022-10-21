27a013b0a69a77b8d0df42a7df8007039a590731
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:48:10 +0530

    Update Role.java

--- BlogAppApi/src/main/java/com/rest/blog/entities/Role.java
+++ BlogAppApi/src/main/java/com/rest/blog/entities/Role.java
@@ -1,1 +1,34 @@
 
+package com.rest.blog.entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+
+import javax.persistence.OneToMany;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+   @Id
+   
+private int id;
+private String name;
+/**One role can have multiple user*/
+@OneToMany(mappedBy="role",cascade= CascadeType.ALL)
+private List<User> user;
+
+}
