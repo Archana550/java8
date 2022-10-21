84c491d99decd5ff113ae8435b384900ff4f1859
Archana Kumari <archanakumari8n7@gmail.com>
2022-10-21 12:26:35 +0530

    Update JwtAuthRequest.java

--- BlogAppApi/src/main/java/com/rest/blog/auth/JwtAuthRequest.java
+++ BlogAppApi/src/main/java/com/rest/blog/auth/JwtAuthRequest.java
@@ -1,1 +1,20 @@
 
+package com.rest.blog.auth;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class JwtAuthRequest {
+
+private String username;
+private String password;
+}
